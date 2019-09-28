64e1a023521b7bbc75c9c197961af4e11302408e
ijgn-build_mac <ijgn-build_mac@kamadayuheinoMacBook-Pro.local>
2019-09-28 14:31:05 +0900

    [add]active adminを導入し、メモ画面を作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_27_113822) do
+ActiveRecord::Schema.define(version: 2019_09_28_020426) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2019_09_27_113822) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "memos", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
