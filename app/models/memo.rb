def02dfead1e485d4dc095abcecd85613d7087c7
ijgn-build_mac <ijgn-build_mac@kamadayuheinoMacBook-Pro.local>
2019-09-29 08:08:34 +0900

    annotate gem を適用し、モデルにスキーマ情報を追加

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: memos
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Memo < ApplicationRecord
   has_many :tags, through: :memo_tags
   has_many :memo_tags
--- app/models/memo_tag.rb
+++ app/models/memo_tag.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: memo_tags
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  memo_id    :bigint
+#  tag_id     :bigint
+#
+# Indexes
+#
+#  index_memo_tags_on_memo_id  (memo_id)
+#  index_memo_tags_on_tag_id   (tag_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (memo_id => memos.id) ON DELETE => cascade
+#  fk_rails_...  (tag_id => tags.id) ON DELETE => cascade
+#
+
 class MemoTag < ApplicationRecord
   belongs_to :memo
   belongs_to :tag
