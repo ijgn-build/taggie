0c41a08a4fd51e72d27f091e1de2555926884c0b
ijgn-build_mac <ijgn-build_mac@kamadayuheinoMacBook-Pro.local>
2019-09-28 14:55:43 +0900

    [update] エラーメッセージを上部へ表示

--- app/admin/memos.rb
+++ app/admin/memos.rb
@@ -1,6 +1,15 @@
 ActiveAdmin.register Memo do
   permit_params :title, :description
 
+  form do |f|
+    # エラー表示枠を表示、シンボルで項目を指定
+    f.semantic_errors :name
+    # モデルの入力項目を表示
+    f.inputs
+    # 登録・更新などのボタンの表示
+    f.actions
+  end
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
