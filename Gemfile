921ff8e7b7c3166173994fe013c54eedaafc6b23
ijgn-build_mac <ijgn-build_mac@kamadayuheinoMacBook-Pro.local>
2019-09-29 11:18:59 +0900

    アクセスしたいサイトのbodyを取得できるように修正

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'jbuilder', '~> 2.5'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry-rails'
 end
 
 group :development do
@@ -67,3 +68,4 @@ gem 'devise-i18n'
 gem 'globalize', github: 'globalize/globalize'
 gem 'pg'
 gem 'annotate'
+gem 'mechanize'
--- app/admin/memos.rb
+++ app/admin/memos.rb
@@ -1,5 +1,34 @@
+require 'mechanize'
 ActiveAdmin.register Memo do
   permit_params :title, :description, memo_tags_attributes: [:memo_id, :tag_id, :name, :_destroy, :_edit]
+
+  controller do
+    layout 'active_admin'
+    def show
+      @mechanize = mechanize
+      render partial: 'show', locals: {page_content: @mechanize}
+      
+      #post_params = permitted_params[:user]
+      #@user = User.new(post_params)
+      ## 何らかのカスタム処理
+      #if @user.save
+      #  # 保存に成功したら一覧画面にリダイレクト
+      #  redirect_to collection_path
+      #else
+      #  # 保存に失敗したらformテンプレート出力
+      #  # validationエラーが表示される
+      #  # layout: active_adminがないとヘッダ・フッタが出力されないので注意
+      #  render "_form", layout: "active_admin"
+      #end
+    end
+    def mechanize
+      agent = Mechanize.new
+      # page = agent.get('http://www.kayac.com/')
+      page = agent.get('https://build.ijgn.jp/')
+      
+      page.at("body").text
+    end
+  end
 
   form do |f|
     # エラー表示枠を表示、シンボルで項目を指定
