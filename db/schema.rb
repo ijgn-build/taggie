d6e4885853d10c6c20103b091407169e7306b0c6
ijgn-build_mac <ijgn-build_mac@kamadayuheinoMacBook-Pro.local>
2019-10-23 01:23:38 +0900

    [update]カラム追加の情報を反映

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_28_145853) do
+ActiveRecord::Schema.define(version: 2019_10_22_073728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2019_09_28_145853) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
   end
 
   create_table "tags", force: :cascade do |t|
